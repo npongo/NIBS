b847ecfdd8828ea85250fafc3500e3bb08012c1e
benjamin d clark <benjamin@npongo.com>
2025-03-11 17:46:08 +0200

    resize images

---
+++ resize_images.py
@@ -0,0 +1,20 @@
+#%%
+from PIL import Image
+import os
+
+def resize_images(directory, scale_factor):
+    for filename in os.listdir(directory):
+        if filename.endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif')):
+            img_path = os.path.join(directory, filename)
+            with Image.open(img_path) as img:
+                new_width = int(img.width * scale_factor)
+                new_height = int(img.height * scale_factor)
+                img = img.resize((new_width, new_height))
+                img.save(img_path)
+                print(f"Resized {filename} to {new_width}x{new_height}")
+
+#%%
+graphs_directory = "d:/Repos/NIBS/graphs"
+scale_factor = 0.5
+resize_images(graphs_directory, scale_factor)
+# %%
